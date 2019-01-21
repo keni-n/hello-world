89b654d6294cc55f33667f9e0b272b68137dbf0f
Kenichi Nagai <kenichi.nagai.ry@renesas.com>
2019-01-21 14:24:37 +0900

    Project initialization

--- src/vis_client_example.py
+++ src/vis_client_example.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 VIS_URL = "wss://wwwivi:8088/"
 
 # Go to https://webhook.site/#/ copy and paste server link to HTTP_REQUEST_RECEIVER_URL
-HTTP_REQUEST_RECEIVER_URL = "<replace with your url>"
+HTTP_REQUEST_RECEIVER_URL = "https://webhook.site/951f36a2-4075-4e23-bd4b-10483ef84189"
 
 DATA_SENDING_DELAY = 2
 WAIT_TIMEOUT = 5
